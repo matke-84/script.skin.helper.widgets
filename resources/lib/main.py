90462c95624265af7adb9994d506ad92d2a22305
matke-84 <74268331+matke-84@users.noreply.github.com>
2023-02-27 11:55:17 +0100

    Update main.py

--- resources/lib/main.py
+++ resources/lib/main.py
@@ -111,8 +111,17 @@ class Main(object):
                 options["mediatype"] = "tvshows"
                 options["random"] = True
 
-        options["skipcache"] = "true"
-
+        # set the widget settings as options
+        options["hide_watched"] = self.addon.getSetting("hide_watched") == "true"
+        if self.addon.getSetting("hide_watched_recent") == "true" and "recent" in options.get("action", ""):
+            options["hide_watched"] = True
+        options["next_inprogress_only"] = self.addon.getSetting("nextup_inprogressonly") == "true"
+        options["episodes_enable_specials"] = self.addon.getSetting("episodes_enable_specials") == "true"
+        options["group_episodes"] = self.addon.getSetting("episodes_grouping") == "true"
+        if "limit" in options:
+            options["limit"] = int(options["limit"])
+        else:
+            options["limit"] = int(self.addon.getSetting("default_limit"))
         return options
 
     def show_widget_listing(self):
